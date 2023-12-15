f24c778e0c33db03631ec199c5b55583e163c139
mrludovicc <nadal46@hotmail.com>
2023-12-14 23:44:09 -0500

    Add back button on nav bar

--- src/components/Navigation/NavigationBar.jsx
+++ src/components/Navigation/NavigationBar.jsx
@@ -1,8 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../scss/navigation.scss";
 
 const NavigationBar = () => {
+  const location = useLocation();
+
+  const showPlanOrBackButton = () => {
+    if (location.pathname === "/") {
+      return "PLAN";
+    } else if (location.pathname === "/stageTwo") {
+      return "Back";
+    } else if (location.pathname === "/stageThree") {
+      return "Back";
+    }
+    return null;
+  };
+
+  const getButtonLink = () => {
+    if (location.pathname === "/stageTwo") {
+      return "/stageOne";
+    } else if (location.pathname === "/stageThree") {
+      return "/stageTwo";
+    }
+    return "/stageOne";
+  };
+
   return (
     <nav className={"nav-bar"}>
       <ul>
@@ -11,9 +33,13 @@ const NavigationBar = () => {
             <span className={"mars-logo"}>MARS</span>
           </Link>
         </li>
-        <Link to="/StageOne">
-          <div className={"plan-button"}>PLAN</div>
-        </Link>
+        {showPlanOrBackButton() && (
+          <li>
+            <Link to={getButtonLink()}>
+              <div className={"plan-button"}>{showPlanOrBackButton()}</div>
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
